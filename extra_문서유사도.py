99eef7123687bfab0ac040bb51d6b14a8dc2050c
kkangwook <send2woohyun@naver.com>
2025-04-24 11:16:29 +0900

    Update extra_문서유사도.py

--- extra_문서유사도.py
+++ extra_문서유사도.py
@@ -38,3 +38,31 @@ newbog=vector.transform(new)
 sim=cosine_similarity(newbog, bog)  # (1,3)  반대로 넣으면 (3,1)
 sim = sim.reshape(3)
 sim_idx = sim.argsort()[::-1]  # 0이 제일 유사한 문장 
+
+
+3.word2vec :단어를 벡터로 표현하며, 의미적 유사성을 잘 반영하도록 학습
+         ->유사 문서나 단어 검색에 이용 ex) “king - man + woman ≈ queen” 같은 연산이 가능
+두가지 방식: CBOW(주변단어통해 중심단어 예측), Skip-Gram(중심단어통해 주변단어예측)-> CBOW보다 더 효율적 
+word2vec은 다음과 같은 형식으로 들어가야함
+sentences = [
+    ['나는', '밥을', '먹었다'],
+    ['그는', '학교에', '갔다'],
+    ['나는', '책을', '읽었다'],
+    ['그녀는', '영화를', '봤다']
+]
+result = [word_tokenize(row) for row in columns.tolist()] 하여 만듬
+
+from gensim.models import Word2Vec
+----파라미터----
+sentences: 데이터
+window: 한번에 학습시킬 단어개수(범위)  (기본값은 5)
+min_count:단어가 최소 몇 번 이상 등장해야 학습에 포함되는지 (기본값은 5)
+size: 단어 벡터의 차원 (기본값은 100)
+sg: 1이면 skip-gram선택, 0이면 CBOW선택
+
+model = Word2Vec(sentences=result, window=5, min_count=1, sg=1)  #모델생성
+
+-유사단어검색-
+word_search = model.wv.most_similar(['밥은'],topn=3) #sentences안에있는 데이터 넣음, topn은 유사 상위 개수
+
+
